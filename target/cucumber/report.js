946532c183eacc5f14c4f90624c315b6625ddf4c
ranjeeth66 <ranjeethgandamala@gmail.com>
2020-09-18 19:46:54 +0530

    POM changes to execute as Maven test

--- target/cucumber/report.js
+++ target/cucumber/report.js
@@ -119,14 +119,14 @@ formatter.match({
   "location": "Steps.user_launches_browser()"
 });
 formatter.result({
-  "duration": 23708573600,
+  "duration": 10360749800,
   "status": "passed"
 });
 formatter.match({
   "location": "Steps.user_opens_google_homepage()"
 });
 formatter.result({
-  "duration": 275133800,
+  "duration": 112645300,
   "status": "passed"
 });
 formatter.match({
@@ -139,14 +139,14 @@ formatter.match({
   "location": "Steps.user_searches_given_data(String)"
 });
 formatter.result({
-  "duration": 23485238900,
+  "duration": 3072221600,
   "status": "passed"
 });
 formatter.match({
   "location": "Steps.user_clicks_on_the_first_record()"
 });
 formatter.result({
-  "duration": 37556059000,
+  "duration": 5481925400,
   "status": "passed"
 });
 formatter.match({
@@ -159,7 +159,7 @@ formatter.match({
   "location": "Steps.verify_the_logo(String)"
 });
 formatter.result({
-  "duration": 67816000,
+  "duration": 54409700,
   "status": "passed"
 });
 });
